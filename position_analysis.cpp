d0afb195f0c5d642d1cab6e45e8c2c1af151120b
kryczko <kevinryczko@me.com>
2013-05-29 15:43:17 -0400

    Initial push of program

---
+++ position_analysis.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+// file streams
+ifstream inputfile;
+ofstream oo_outputfile, oh_outputfile, angle_outputfile;
+
+char oodistance, ohdistance, angle; 
+
+// main menu for the program
+cout << "\n\n     Welcome to the bulk-water data analysis program!\n\n";
+cout << "     If you would like to plot the average O-O distance with respect to timesteps enter \"y\", if not enter \"n\": ";
+cin >> oodistance;
+cout << "     If you would like to plot the average O-H distance with respect to timesteps enter \"y\", if not enter \"n\": ";
+cin >> ohdistance;
+cout << "     If you would like to plot the average H-O-H angle with respect to timesteps enter \"y\", if not enter \"n\": ";
+cin >> angle;
+// end of main menu
+
+//decision for oodistance
+if (oodistance == 'y')
+{
+	oo_outputfile.open("oo_avg_distance.dat")
+
+
+	cout << "\n\nYour O-O average distance data with respect to timesteps has been placed in \"oo_avg_distance.dat\" and can now be easily plotted with gnuplot.\n\n"; 
+}
+else
+{
+	cout << "\n\nYou either entered the wrong key or you do not want to plot the average O-O distance.\n\n";
+}
+
+//decision for ohdistance
+if (ohdistance == 'y')
+{
+	oh_outputfile.open("oh_avg_distance.dat")
+
+
+        cout << "\n\nYour O-H average distance data with respect to timesteps has been placed in \"oh_avg_distance.dat\" and can now be easily plotted with gnuplot.\n\n";
+}
+else
+{
+        cout << "\n\nYou either entered the wrong key or you do not want to plot the average O-H distance.\n\n";
+}
+
+//decision for angle
+if (angle == 'y')
+{
+        angle_outputfile.open("avg_angle.dat")
+
+
+        cout << "\n\nYour H-O-H average angle data with respect to timesteps has been placed in \"avg_angle.dat\" and can now be easily plotted with gnuplot.\n\n";
+}
+else
+{
+        cout << "\n\nYou either entered the wrong key or you do not want to plot the average H-O-H angle.\n\n";
+}
+
+
+
+
+
+
+
+
+
+return 0;
+}
